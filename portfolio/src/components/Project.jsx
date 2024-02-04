6371037d70db9aee0ed2b98e8c78de33f1a0c8a3
yuzbamaria <54204342+yuzbamaria@users.noreply.github.com>
2024-02-04 12:31:03 +0000

    updated Projects page

--- portfolio/src/components/Project.jsx
+++ portfolio/src/components/Project.jsx
@@ -1,23 +1,27 @@
 function Project(props) {
     return <div>
-        <div className="card col col-xs-10 col-sm-10 col-md-5 col-lg-3 m-2 p-3">
-            <h2 className="card-title">{ props.title }</h2>
-            <p>{ props.description }</p>
-            <img src={props.image} />
+          <div className='card align-items-center shadow'>
+            <div className='card-body text-center'>
+              <h2 className="card-title">{ props.title }</h2>
+              <p>{ props.description }</p>
+                <img 
+                src={props.image}
+                alt={props.title}
+                />
             <div className="custom-card-footer">
                 <p>
-                    <button onClick={() => window.open(props.deployed, '_blank')}>
+                    <button className='btn btn-color shadow' onClick={() => window.open(props.deployed, '_blank')}>
                         Deploy link
                     </button>
-                    <button onClick={() => window.open(props.github, '_blank')}>
+                    <button className='btn btn-color shadow m-2' onClick={() => window.open(props.github, '_blank')}>
                         Github Repo
                     </button>
                 </p>
             </div>
         </div>
-
-        
+      </div>
     </div>
 }
+
 
 export default Project; 
--- portfolio/src/pages/ProjectsGallery.jsx
+++ portfolio/src/pages/ProjectsGallery.jsx
@@ -3,15 +3,14 @@ import projects from '../data/projects.json';
 
 function ProjectsGallery() {
     return <div>
-        <div className="background min-vh-100 pb-5">
+         <div className="row justify-content-center"></div>
             <h5  className="p-5 mb-3">This is the list of my projects</h5>
-            <div className="flex-wrap d-flex m-4 p-4">
+            <div className="row row-cols-1 row-cols-md-1 row-cols-lg-2 p-3 m-3">
              { projects.map((project) => {
                 return <Project key={project.id} title={project.title} image={project.image} description={project.description} deployed={project.deployed} github={project.github}/>            
              })}
             </div>
         </div>
-    </div>
 }
 
 export default ProjectsGallery;
